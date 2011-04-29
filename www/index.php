faf6b7259f4490fb33bcee0d25d436c8e0e07c56
saturngod <saturngod@gmail.com>
2011-04-29 09:59:57 +0800

    change web directory for security

--- www/index.php
+++ www/index.php
@@ -11,11 +11,12 @@
  error_reporting(E_ALL);
 
  /*** define the site path constant ***/
- $site_path = realpath(dirname(__FILE__));
+ $site_path = realpath(substr(dirname(__FILE__),0,-4));
+
  define ('SITE_PATH', $site_path."/system");
  
  
  /*** include the init.php file ***/
- include 'system/includes/init.php';
+ include SITE_PATH.'/includes/init.php';
 
 ?>
