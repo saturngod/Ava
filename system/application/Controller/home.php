1bdaf040f1f93e7e203953193a47a06f7c22946c
saturngod <saturngod@gmail.com>
2012-01-24 12:02:14 +0800

    fixed home controller

--- system/application/Controller/home.php
+++ system/application/Controller/home.php
@@ -7,23 +7,7 @@ class Home extends Ava_Controller {
 	 */
 	function index()
 	{
-		/*
-		SELECT * FROM `checkin`
-        GROUP BY `user_id`
-        HAVING (rate) > 1
-        */
-        $this->db->select("AVG(rate)");
-        $this->db->group_by("club_id");
-        $this->db->having("AVG(rate) > 2");
-		$result=$this->db->get("checkin");
-
-		echo "<pre> SQL:";
-		print_r($this->db->sql);
-		echo "</pre>";
-
-		echo "<pre>Result:";
-		print_r($this->db->count());
-		echo "</pre>";
+		$this->load->view('home');
     }
 }
 
